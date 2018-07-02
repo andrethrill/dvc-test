a9f6a93ee01b737f5784b12ce5038261a20d770e
Andre Silva <andrethrilldev@gmail.com>
2018-07-02 12:14:20 +0100

    updated script to gen v2 by appending to the content of data v1

--- src/makev2.py
+++ src/makev2.py
@@ -2,7 +2,10 @@ import os
 
 print(os.getcwd())
 
+lines = '\n'.join(open('../data/data_v1.txt', 'r').readlines())
+
 with open('../data/data_v2.txt', 'w') as file:
+	file.write(lines)
 	file.write("I am v2")
 
 
